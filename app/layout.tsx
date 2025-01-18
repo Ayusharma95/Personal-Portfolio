59b1701cbe6ac714e76d24d93fb485f12b85f3e8
Ayusharma95 <ayushtejpalsharma@gmail.com>
2025-01-18 14:42:35 +0530

    Add favicon files for multiple sizes and platforms

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Added favicon */}
-        <link rel="icon" href="public\ATS.ico" type="image/x-icon" />
+        {/* Added favicon with multiple sizes for better appearance */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-64x64.png" sizes="64x64" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/android-chrome-192x192.png" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/android-chrome-512x512.png" sizes="512x512" type="image/png" />
       </head>
       <body className={`${inter.className} dark`}>
         <ThemeProvider
